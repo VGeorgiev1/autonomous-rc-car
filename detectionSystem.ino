642c214e54b156072ac13d1bd1c8471aa41972bc
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2018-04-27 23:01:32 +0300

    something new
    almost working

--- detectionSystem.ino
+++ detectionSystem.ino
@@ -1,51 +1,66 @@
-const int trigPin = 10;
-const int echoPin = 11;
+#include <NewPing.h>
 
+NewPing sonar1(13, 12, 100); // Sensor 1: trigger pin, echo pin, maximum distance in cm
+NewPing sonar2(5, 6, 100); // Sensor 2: same stuff
+#define pingSpeed 100
 const int motorPin1  = 4;  // Pin 14 of L293
 const int motorPin2  = 3;  // Pin 10 of L293
-const int enA = 9;
 //Motor B
-const int motorPin3  = 6; // Pin  7 of L293
-const int motorPin4  = 7;  // Pin  2 of L293
-const int enB = 8;
+const int motorPin3  = 7; // Pin  7 of L293
+const int motorPin4  = 8;  // Pin  2 of L293
+const int enB = 9;
+unsigned long pingTimer1, pingTimer2;
+int in1, in2;
 // defines variables
 long duration;
+long duration2;
 int distance;
-int pwm = 200;
+int distance2;
+int pwm = 170;
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pingTimer1 = millis() + pingSpeed; // Sensor 1 fires after 100ms (pingSpeed)
+  pingTimer2 = pingTimer1 + (pingSpeed / 2); // Sensor 2 fires 50ms later
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
   pinMode(enB, OUTPUT);
+  Serial.begin(9600);
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW); 
 }
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(4);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance= duration*0.034/2;
-  if(distance < 30){
+ 
+  if (millis() >= pingTimer1) {
+     pingTimer1 += pingSpeed; // Make sensor 1 fire again 100ms later (pingSpeed)
+     in1 = sonar1.ping_in();
+  }
+  if (millis() >= pingTimer2) {
+     pingTimer2 = pingTimer1 + (pingSpeed / 2); // Make sensor 2 fire again 50ms after sensor 1 fires
+     in2 = sonar2.ping_in();
+     // Both sensors pinged, process results here
+  }
+ Serial.print("Sensor1: ");
+ Serial.print(in1);
+ Serial.println();
+ Serial.print("Sensor2: ");
+ Serial.print(in2);
+ Serial.println();
+  if((in1 < 40|| in2 < 40) && (in1 != 0 || in2 != 0)){
     if(digitalRead(motorPin2) == HIGH){
-      analogWrite(enA, 0);
+      analogWrite(enB, 0);
       delay(700);
     }else{
       digitalWrite(motorPin2, HIGH);
       digitalWrite(motorPin1, LOW);
-      analogWrite(enA, 180);
+      analogWrite(enB, pwm + 70);
       delay(350);
-      analogWrite(enA, 0);
+      analogWrite(enB, 0);
     }    
-  }else{
+  }else if(in1 == 0 || in2 == 0){
     digitalWrite(motorPin1, HIGH);
     digitalWrite(motorPin2, LOW);
-    analogWrite(enA, 180);
+    analogWrite(enB, pwm);
   }
  
 }
